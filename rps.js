b6270b4d2c92a6d5262443426a699d750ce4359a
Tom Nickless <tcnickless@gmail.com>
2023-11-26 23:22:33 +0000

    implemented 5 round logic

--- rps.js
+++ rps.js
@@ -1,12 +1,4 @@
-let computerSelection = getComputerChoice();
-let playerSelection = prompt("Please choose Rock, Paper, or Scissors: ");
-playerSelection = playerSelection.toLowerCase();
-let roundResult = playerCompare(playerSelection, computerSelection);
-
-
-console.log("Your Opponent Chose: " + computerSelection);
-console.log("You chose: " + playerSelection);
-console.log("Result: " + roundResult);
+game();
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
@@ -21,15 +13,51 @@ function getComputerChoice() {
     return gameString;
 }
 
-function playRound (player, computer) {
+function playRound () {
+    let computerSelection = getComputerChoice();
+    let playerSelection = prompt("Please choose Rock, Paper, or Scissors: ");
+    playerSelection = playerSelection.toLowerCase();
+
+
     let result = "";
-    if (player === computer) {
-        result = "Draw";
-    } else if ((player === "rock" && computer === "paper") || (player === "paper" && computer === "scissor") || (player === "scissor" && computer === "rock")) {
-        result = "Computer Wins!";
-    } else if ((player === "rock" && computer === "scissor") || (player === "paper" && computer === "rock") || (player === "scissor" && computer === "paper")) {
-        result = "You Win!";
+    if (playerSelection === computerSelection) {
+        result = 0;
+    } else if ((playerSelection === "rock" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "scissor") || (playerSelection === "scissor" && computerSelection === "rock")) {
+        result = 1;
+    } else if ((playerSelection === "rock" && computerSelection === "scissor") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissor" && computerSelection === "paper")) {
+        result = 2;
     }
 
     return result;
+}
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let roundResult = playRound();
+
+        if (roundResult === 0) {
+            console.log("This round was a draw!");
+        } else if (roundResult === 1) {
+            console.log("You lost this round!");
+            computerScore += 1;
+        } else if (roundResult === 2) {
+            console.log("You won this round!");
+            playerScore += 1;
+        }
+        console.log("The current score is:");
+        console.log("Player: " + playerScore);
+        console.log("Computer: " + computerScore);
+    }
+
+    console.log("After 5 rounds:")
+    if (playerScore > computerScore) {
+        console.log("THE PLAYER HAS WON");
+    } else if (playerScore < computerScore) {
+        console.log("THE COMPUTER HAS WON");
+    } else {
+        console.log("IT WAS A DRAW!!!");
+    }
 }

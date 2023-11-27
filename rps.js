300d53c1a3d493356c0d7e2555a4337328dadeb9
Tom Nickless <tcnickless@gmail.com>
2023-11-27 12:31:01 +0000

    adding detailed comments for later review of code

--- rps.js
+++ rps.js
@@ -1,7 +1,9 @@
 game();
 
 function getComputerChoice() {
+    // generate random number 1-3 and sanitise it
     let choice = Math.floor(Math.random() * 3);
+    // convert random number to human readable game state
     if (choice === 0) {
         gameString = "rock";
     } else if (choice === 1) {
@@ -14,11 +16,17 @@ function getComputerChoice() {
 }
 
 function playRound () {
+    // fetch game inputs ready for evaluation
     let computerSelection = getComputerChoice();
     let playerSelection = prompt("Please choose Rock, Paper, or Scissors: ");
+    // sanitise human inputs to lower case for string comparisons
     playerSelection = playerSelection.toLowerCase();
 
-
+    // 0 = draw
+    // 1 = lose
+    // 2 = win
+    // compare game states and output result value
+    // state permutations blocked by result
     let result = "";
     if (playerSelection === computerSelection) {
         result = 0;
@@ -32,12 +40,17 @@ function playRound () {
 }
 
 function game() {
+    // initialise/reset scores for game
     let playerScore = 0;
     let computerScore = 0;
 
+    // loop through 5 rounds
     for (let i = 0; i < 5; i++) {
+        // store evaluated game state
         let roundResult = playRound();
 
+        // convert evaluated game state to human readable form and output to console
+        // start with individual round update, and increment overall score appropriately
         if (roundResult === 0) {
             console.log("This round was a draw!");
         } else if (roundResult === 1) {
@@ -47,11 +60,13 @@ function game() {
             console.log("You won this round!");
             playerScore += 1;
         }
+        // summarise overall current/temporary score to user via console message
         console.log("The current score is:");
         console.log("Player: " + playerScore);
         console.log("Computer: " + computerScore);
     }
-
+    // once game is complete, summarise final score
+    // basic win detection using score comparisons
     console.log("After 5 rounds:")
     if (playerScore > computerScore) {
         console.log("THE PLAYER HAS WON");
